683518ced4b084ac7684c9854124be49c48fac80
Chester <atomiccoder91@gmail.com>
2023-10-28 13:31:20 +0100

    0.3.1

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,27 +3,19 @@
 #include "../ansijson.h"
 #include "helpers.h"
 
-#include "../lib/cJSON.c"
-
-
-#include <time.h>
-clock_t start_time, end_time;
-double cpu_time_used;
-
-
 int main(int argc, char *argv[])
 {
-    char *file = readFile("D:\\Dev\\ANSIJSON\\examples\\json\\DamagedHelmet.gltf");
+    // "Hello World!"
+    struct aJSON *example1_data = decodeAJSON(readFile("../examples/json/hello_world.json"));
+    example1_data->next->append("{\"one_more\": \"!\"}");
+    std::cout << "Example 1:\n" << example1_data->encode() << "\n" << std::endl;
+    freeAJSON(example1_data); // Deallocate
 
-    start_time = clock();
-    cJSON *cjson = cJSON_Parse(file);
-    end_time = clock();
-    std::cout << "cJSON Completed in " << (((double)(clock() - start_time) * 1000.0) / CLOCKS_PER_SEC) << "ms" << std::endl;
+    // Print a slightly more complex JSON file containing dimensions, floats, unicodes and more
+    std::cout << "Example 2:\n" << decodeAJSON(readFile("../examples/json/complex.json"))->encode(1) << "\n" << std::endl;
 
-    start_time = clock();
-    aJSON *ajson = decodeAJSON(file);
-    end_time = clock();
-    std::cout << "aJSON Completed in " << (((double)(clock() - start_time) * 1000.0) / CLOCKS_PER_SEC) << "ms" << std::endl;
+    // Print a GLTF file containing embedded geometry for a square
+    std::cout << "Example 3:\n" << decodeAJSON(readFile("../examples/json/box.gltf"))->encode(1) << "\n" << std::endl;
 
     return 0;
 }
